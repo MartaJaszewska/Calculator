041078f01194712db775743f49f997b57e1bdb6f
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-12-22 01:33:27 +0100

    map method instead of for loops, round and font size helper functions, event listeners added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let number1 = 0,
     number2 = 0,
     operator = '',
-    isOperatorClicked = false, //true when in expression some operator was clicked at least once
-    equalAgain = ''; //when '=' is click again, used to repeat previous operation
+    isOperatorClicked = false, // returns true when an operator was clicked at least once in the expression
+    isEqualsClicked = false, // returns true when an equals was clicked at least once in the expression
+    equalsAgain = ''; //when the '=' is clicked again this variable is used to repeat the previous operation
 
 const numbers = document.querySelectorAll('.btn-number'), // 1-9 buttons
       operators = document.querySelectorAll('.btn-operator'), // '+','-', '×', '÷' buttons
@@ -10,91 +11,74 @@ const numbers = document.querySelectorAll('.btn-number'), // 1-9 buttons
       clearEntry = document.getElementById('btn-clearEntry'), // 'CE' button
       opposite = document.getElementById('btn-opposite'), // '+/-' button
       decimal = document.getElementById('btn-decimal'), // '.' button
-      equal = document.getElementById('btn-equal'), // '=' button
+      equals = document.getElementById('btn-equals'), // '=' button
       input = document.getElementById('input');
 
-clearEntry.onclick = () => {
-  if (input.value !== 'error') input.value = input.value.slice(0, -1);
-};
+const onClearEntry = () => {
+  if (input.value !== 'error' && !isEqualsClicked && !isOperatorClicked) {
+    input.value = input.value.slice(0, -1);
+    font(input);
+}};
 
-clear.onclick = () => {
+const onClear = () => {
   input.value = '0';
+  font(input);
   number1 = 0;
   number2 = 0;
   operator = '';
   isOperatorClicked = false;
-  font();
+  isEqualsClicked = false;
 };
 
-opposite.onclick = () => {
+const onOpposite = () => {
   if (input.value === 'error') return;
-  input.value = -input.value;
-}
-
-decimal.onclick = () => {
-  if (input.value.indexOf('.') === -1 && input.value !== 'error') input.value += '.';
+  input.value = -1 * input.value;
+  font(input);
 };
 
-equal.onclick = () => {
-  font();
+const onDecimal = () => {
+  if (input.value.indexOf('.') === -1 && input.value !== 'error' && !isEqualsClicked) {
+    input.value += '.';
+    font(input);
+}};
+
+const onEquals = () => {
   if (input.value === 'error') return;
-  if (operator) {
+  if (operator && !isEqualsClicked) {
     number2 = parseFloat(input.value);
-    if (divisionError()) return;
-    else {
-      input.value = calculate(operator, number1, number2);
-      equalAgain = operator;
-      operator = '';
-      isOperatorClicked = false;
-    }
-  } else {
-    input.value = calculate(equalAgain, parseFloat(input.value), number2);
-  }
+    if (divisionError(operator, number2, input)) return;
+    input.value = calculate(operator, number1, number2);
+    round(input);
+    font(input);
+    equalsAgain = operator;
+    operator = '';
+    isOperatorClicked = false;
+    isEqualsClicked = true;
+  } else if (!operator && isEqualsClicked) {
+    input.value = calculate(equalsAgain, parseFloat(input.value), number2);
+    round(input);
+    font(input);
+}};
+
+const font = (input) => { // changes the font size depending on the length of the expression
+  if (input.value.length < 7) input.style.fontSize = '60px';
+  else if (input.value.length < 9) input.style.fontSize = '50px';
+  else if (input.value.length < 11) input.style.fontSize = '40px';
+  else if (input.value.length < 13) input.style.fontSize = '30px';
+  else if (input.value.length < 15) input.style.fontSize = '25px';
+  else if (input.value.length < 17) input.style.fontSize = '20px';
+  else if (input.value.length > 20) {
+    input.value = 'error';
+    font(input);
+    console.error('Maximum expression length has been reached.')
+}};
+
+const round = (input) => { // rounds the result to the set number of decimal places
+  if (input.value.includes('.')) input.value = parseFloat(input.value).toFixed(2);
 };
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].onclick = () => {
-    font();
-    if (input.value === 'error') return;
-
-    if (isOperatorClicked) {
-      number1 = parseFloat(input.value);
-      input.value = '';
-    }
-    if (input.value === '0') {
-      input.value = numbers[i].innerText;
-      isOperatorClicked = false;
-    } else {
-      input.value += numbers[i].innerText;
-      isOperatorClicked = false;
-    }
-  };
-}
-
-for (let i = 0; i < operators.length; i++) {
-  operators[i].onclick = () => {
-    font();
-    if (input.value === 'error') return;
-    if (!operator) {
-      number1 = parseFloat(input.value);
-      operator = operators[i].innerText;
-      isOperatorClicked = true;
-    } else {
-      number2 = parseFloat(input.value);
-      if (divisionError()) return;
-      else {
-        input.value = calculate(operator, number1, number2);
-        operator = operators[i].innerText;
-        number2 = parseFloat(input.value);
-        isOperatorClicked = true;
-      }
-    }
-  };
-}
-
-const calculate = (operator, number1, number2) => {
+const calculate = (operator, number1, number2) => { // math operations
   let result = 0;
-
   switch (operator) {
     case '+':
       result = number1 + number2;
@@ -112,21 +96,51 @@ const calculate = (operator, number1, number2) => {
   return result;
 };
 
-const divisionError = () => {
+const divisionError = (operator, number2, input) => { // insurance division by zero
   if (operator === '÷' && number2 === 0) {
     input.value = 'error';
     console.error('Division by 0 is undefined.');
     return true;
-  }
-};
+}};
 
-const font = () => {
-  if (input.value.length < 6) input.style.fontSize = '60px';
-  else if (input.value.length < 8) input.style.fontSize = '50px';
-  else if (input.value.length < 10) input.style.fontSize = '40px';
-  else if (input.value.length < 12) input.style.fontSize = '30px';
-  else if (input.value.length < 14) input.style.fontSize = '20px';
-  else if (input.value.length > 20) {
-    console.error('Maximum expression length reached.')
-    return input.value = 'error'}
-}
+clearEntry.addEventListener('click', onClearEntry);
+clear.addEventListener('click', onClear);
+opposite.addEventListener('click', onOpposite);
+decimal.addEventListener('click', onDecimal);
+equals.addEventListener('click', onEquals);
+
+Array.from(numbers).map((num) => {
+  num.addEventListener('click', () => {
+    if (input.value === 'error') return;
+    if (isOperatorClicked) {
+      number1 = parseFloat(input.value);
+      input.value = '';
+    }
+    if (input.value === '0') {
+      input.value = num.innerText;
+      isOperatorClicked = false;
+      isEqualsClicked = false;
+    } else if (isEqualsClicked === false || isOperatorClicked === true) {
+      input.value += num.innerText;
+      font(input);
+      isOperatorClicked = false;
+      isEqualsClicked = false;
+}})});
+
+Array.from(operators).map((operation) => {
+  operation.addEventListener('click', () => {
+    if (input.value === 'error') return;
+    if (!operator) {
+      number1 = parseFloat(input.value);
+      operator = operation.innerText;
+      isOperatorClicked = true;
+    } else {
+      number2 = parseFloat(input.value);
+      if (divisionError(operator, number2, input)) return;
+      else {
+        input.value = calculate(operator, number1, number2);
+        font(input);
+        operator = operation.innerText;
+        number2 = parseFloat(input.value);
+        isOperatorClicked = true;
+}}})});
